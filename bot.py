15c09225a4895fc19f451bc0543d844c4300ee05
雪浩 <james10949@users.noreply.github.com>
2020-06-06 07:50:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,15 @@ bot = commands.Bot(command_prefix = "$")
 @bot.event
 
 async def on_ready():
-    print(">> Bot is online <<")
+    print(">> System Online <<")
 
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(710718810470088709)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(710718810470088709)
+    await channel.send(f'{member} leave!')
 bot.run('NzE4NTI2NjEwMzAyNjk3NDky.XtqKOA.oqmcS95eIfgcPUPUqYD0ZBR8BBA')
