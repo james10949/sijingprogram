895c5f32dd1d101cecc3230fa78f6778c793a16f
雪浩 <james10949@users.noreply.github.com>
2020-06-06 14:29:33 +0800

    update tasks.py

--- cmds/tasks.py
+++ cmds/tasks.py
@@ -10,19 +10,46 @@ class Tasks(Cog_Extension):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-		async def interval():
+		self.counter = 0
+
+		# async def interval():
+		# 	await self.bot.wait_until_ready()
+		# 	self.channel = self.bot.get_channel(710718810470088712)
+		# 	while not self.bot.is_closed():
+		# 		await self.channel.send('shut the f**k up')
+		# 		await asyncio.sleep(5)
+
+		async def time_task():
 			await self.bot.wait_until_ready()
 			self.channel = self.bot.get_channel(710718810470088712)
 			while not self.bot.is_closed():
-				await self.channel.send('shut the f**k up')
-				await asyncio.sleep(5)
+				now_time = datetime.datetime.now().strftime('%H%M')
+				with open('settings.json', 'r', encoding='utf8') as settings:
+					botconfig = json.load(settings)
+				if now_time == botconfig['time'] and self.counter == 0:
+					await self.channel.send("It's time...")
+					self.counter = 1
+					await asyncio.sleep(1)
+				else:
+					await asyncio.sleep(1)
+					pass
 
-		self.bg_task = self.bot.loop.create_task(interval())
+		self.bg_task = self.bot.loop.create_task(time_task())
 
 	@commands.command()
 	async def set_channel(self, ctx, ch:int):
 		self.channel = self.bot.get_channel(ch)
 		await ctx.send(f'Set Channel : {self.channel.mention}')
+
+	@commands.command()
+	async def set_time(self, ctx, time):
+		with open('settings.json', 'r', encoding='utf8') as settings:
+			botconfig = json.load(settings)
+		
+		botconfig['time'] = time
+		with open('settings.json', 'w', encoding='utf8') as settings:
+			json.dump(botconfig, settings, indent=4)
+
 
 def setup(bot):
 	bot.add_cog(Tasks(bot))
