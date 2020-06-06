d930ab97e34de4ad5aaf630a7f370c8a61b0cdea
雪浩 <james10949@users.noreply.github.com>
2020-06-06 08:00:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,9 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(710718810470088709)
     await channel.send(f'{member} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} ms')
+
 bot.run('NzE4NTI2NjEwMzAyNjk3NDky.XtqKOA.oqmcS95eIfgcPUPUqYD0ZBR8BBA')
