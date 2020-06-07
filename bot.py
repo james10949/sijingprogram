e024757707bdfa05f93e0b19e020da8cee590edb
雪浩 <james10949@users.noreply.github.com>
2020-06-07 16:27:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,16 @@ bot = commands.Bot(command_prefix = "$")
 async def on_ready():
 	print(">> System Online <<")
 
+bot.remove_command('help')
+@bot.command()
+async def help(ctx):
+	embed=discord.Embed(title="Help Page", url="https://github.com/james10949/sijingprogram/blob/master/README.md#Commands")
+	embed.add_field(name="Moderations", value="$clean <int\> 清除訊息", inline=False)
+	embed.add_field(name="Utilities", value="$about 顯示關於頁面\n$ping 顯示延遲\n$said <msg\> 機器人代發訊息", inline=False)
+	embed.add_field(name="Funs", value="$picture 顯示隨機圖片(滿滿鈴鈴)", inline=False)
+	embed.add_field(name="Others", value="$help 顯示此頁面", inline=False)
+	await ctx.send(embed = embed)
+
 @bot.command()
 async def load(ctx, extension):
 	bot.load_extension(f'cmds.{extension}')
@@ -27,6 +37,13 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
 	bot.reload_extension(f'cmds.{extension}')
 	await ctx.send(f'Reloaded {extension} done')
+
+@bot.command()
+async def menu(ctx):
+	embed=discord.Embed(title='Invite', url='https://discord.com/api/oauth2/authorize?client_id=718526610302697492&permissions=8&scope=bot', color=0x39c5bb)
+	embed.set_author(name='Menu')
+	embed.set_footer(text='Powered By 夜雲工作室：ユキ綾乃')
+	await ctx.send(embed=embed)
 
 for filename in os.listdir('./cmds'):
 	if filename.endswith('.py'):
